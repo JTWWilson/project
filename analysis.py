7275d7cba7803f676daa3b489716e4b682cf50b6
Jacob <jacobtwwilson@gmail.com>
2022-12-21 16:17:38 +0000

    Make generic get_all_addresses function

--- analysis.py
+++ analysis.py
@@ -1,32 +1,47 @@
 import pyshark
+import typing
+from typing_extensions import Literal
 
 # Convert to iterable for efficiency
-pcap = iter(pyshark.FileCapture('firstRead.pcap'))
+pcap = pyshark.FileCapture('firstRead.pcap')
 
 
-def has_ip_layer(packet: pyshark.packet.packet.Packet) -> bool:
+def has_given_layer(
+    packet: pyshark.packet.packet.Packet, 
+    layer: Literal["ETH", "IP"] = "ETH") -> bool:
     try:
-        ip = packet['IP']
+        packet[layer]
         return True
     except KeyError:
         return False
 
-ip_addresses = set()
 
-i = 0
-while True: #i < 50:
-    try:
-        packet: pyshark.packet.packet.Packet = next(pcap) 
-    except StopIteration:
-        break
-    if has_ip_layer(packet):
-        # print(packet.layers)
-        ip_src = packet.layers[1].src
-        ip_dst = packet.layers[1].dst
-        for addr in (ip_src, ip_dst):
-            if addr not in ip_addresses:
-                ip_addresses.add(addr)
+def get_all_addresses(
+    pcap: pyshark.FileCapture, 
+    address_layer: Literal["ETH", "IP"] = "ETH"
+    ) -> typing.Set[str]:
+    iterable_pcap = iter(pcap)
+    addresses = set()
+    LAYER_NAME_TO_INDEX = {"ETH" : 0, "IP" : 1}
+    layer_index = LAYER_NAME_TO_INDEX[address_layer]
 
-    i += 1
+    i = 0
+    while True: #i < 50:
+        try:
+            packet: pyshark.packet.packet.Packet = next(iterable_pcap) 
+        except StopIteration:
+            break
+        if has_given_layer(packet, address_layer):
+            # print(packet.layers)
+            src = packet.layers[layer_index].src
+            dst = packet.layers[layer_index].dst
+            for addr in (src, dst):
+                if addr not in addresses:
+                    addresses.add(addr)
 
-print(ip_addresses)
+        i += 1
+
+    return addresses
+
+
+print(get_all_addresses(pcap, "ETH"))
