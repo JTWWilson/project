2b94850c853e5cff4b181263469097a039d9a93a
Jacob <jacobtwwilson@gmail.com>
2023-03-06 08:51:50 +0000

    create Network class and make get devices static

--- analysis.py
+++ analysis.py
@@ -17,7 +17,6 @@ cursor = db.cursor()
 cursor.execute("SHOW DATABASES")
 print(type(cursor.fetchall()))
 """
-# Convert to iterable for efficiency
 pcap = pyshark.FileCapture('firstRead.pcap')
 
 
@@ -32,6 +31,41 @@ class Device:
 class DeviceEncoder(json.JSONEncoder):
         def default(self, o):
             return o.__dict__
+
+
+class Network:
+    def __init__(self, devices=()) -> None:
+        self.devices = devices
+
+    @staticmethod
+    def get_devices_from_pcap(pcap: pyshark.FileCapture):
+        """Turns a pcap into a list of Devices"""
+        iterable_pcap = iter(pcap)
+        device_list = []
+
+        while True:
+            try:
+                packet: pyshark.packet.packet.Packet = next(iterable_pcap) 
+            except StopIteration:
+                break
+            if has_given_layer(packet, "ETH"):
+                src_mac = packet.layers[0].src
+                dst_mac = packet.layers[0].dst
+                # If source device is new, create Device object and add it to ret
+                if src_mac not in [d.MAC_ADDRESS for d in device_list]:
+                    new_device = Device(src_mac)
+                    if has_given_layer(packet, "IP"):
+                        new_device.ip_addresses.append(packet.layers[1].src)
+                    # If destination device is also new, create Device object and add it to ret
+                    if dst_mac not in [d.MAC_ADDRESS for d in device_list]:                   
+                        other_new_device = Device(dst_mac)
+                        if has_given_layer(packet, "IP"):
+                            other_new_device.ip_addresses.append(packet.layers[1].dst)
+                        new_device.devices_sent_to.append(other_new_device)
+                        other_new_device.devices_sent_to.append(new_device)
+                    device_list.append(new_device)
+
+        return device_list
 
 
 def has_given_layer(
@@ -106,10 +140,10 @@ def list_ips_by_mac(
     return ret
 
 
-d = Device('ab:ac:ac')
-d2 = Device('ac:ad:ad')
-d.devices_received_from = [d2]
-print(DeviceEncoder().encode(d))
+# # d = Device('ab:ac:ac')
+# # d2 = Device('ac:ad:ad')
+# # d.devices_received_from = [d2]
+# # print(DeviceEncoder().encode(d))
 
 #with open('export.json','w') as f:
 #    f.write(json.dumps(list_ips_by_mac(pcap), indent=4, cls=DeviceEncoder))
