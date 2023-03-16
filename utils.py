2ec5ec9035e681b4aa4080b1325ebe18c6067d30
Jacob <jacobtwwilson@gmail.com>
2023-03-16 15:08:56 +0000

    IP addresses are split by dots not commas

--- utils.py
+++ utils.py
@@ -2,6 +2,6 @@ def is_local_ip_address(ip_address: str) -> bool:
     """Returns True if the ip address is in the local address range."""
     if ip_address.startswith('10.') or ip_address.startswith('192.168'):
         return True
-    if 16 <= int(ip_address.split(',')[1]) < 32:
+    if 16 <= int(ip_address.split('.')[1]) < 32:
         return True
     return False
