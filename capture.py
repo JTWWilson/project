41d2653043f8bfdde66e7548b90d9f91784ce4c9
Jacob <jacobtwwilson@gmail.com>
2023-03-18 18:42:03 +0000

    Try to implement a walk of the local IP addresses

--- capture.py
+++ capture.py
@@ -1,5 +1,7 @@
 import pyshark
 from datetime import datetime
+from scapy.all import sr1, Ether, ARP
+import threading
 
 def capture(output_file=None):
     # Default name for file is the date and time of its capture
@@ -7,7 +9,21 @@ def capture(output_file=None):
         now = datetime.now()
         output_file = "{}-{}-{}_{}-{}-{}.pcap".format(now.year, now.month, now.day, now.hour, now.minute, now.second)
     cap = pyshark.LiveCapture(output_file=output_file, interface="WiFi")
+    # threading.Thread(target=walk_local_ipv4, daemon=True).start()
     cap.sniff(timeout=60)
+
+
+def send_arp(ip: str):
+    print(sr1(Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=ip), timeout=3))
+
+
+
+def walk_local_ipv4():
+    ip_base = "192.168"
+    for a in range(256):
+        for b in range(256):
+            send_arp("{}.{}.{}".format(ip_base,a,b))
+
 
 if __name__ == "__main__":
     capture()
