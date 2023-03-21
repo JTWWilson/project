bfc9bb872681016fa1df7ea8c08eb9ddc3cc3112
Jacob <jacobtwwilson@gmail.com>
2023-03-21 17:47:21 +0000

    Add field identifying routers to db

--- device_db_manager.py
+++ device_db_manager.py
@@ -1,3 +1,4 @@
+import re
 import socket
 import sqlite3
 from utils import get_manufacturer_from_mac, is_reserved_mac_address
@@ -6,7 +7,7 @@ from cryptography import x509
 
 DEFAULT_DB_NAME = 'devices.db'
 
-def ensure_table_exists(connection: sqlite3.Connection) -> None:
+def ensure_device_table_exists(connection: sqlite3.Connection) -> None:
     connection.execute("""
         CREATE TABLE IF NOT EXISTS DEVICES (
             mac TEXT NOT NULL PRIMARY KEY,
@@ -20,13 +21,14 @@ def ensure_table_exists(connection: sqlite3.Connection) -> None:
 def get_device_name(mac: str, ip: str =None, device_db=DEFAULT_DB_NAME) -> str:
     """Returns a human readable name for a device from a mac address"""
     with sqlite3.connect(device_db) as connection:
-        ensure_table_exists(connection)
+        ensure_device_table_exists(connection)
         # Look for the MAC address in the device database
         name = connection.execute("SELECT name FROM DEVICES WHERE mac = '{}';".format(mac)).fetchall()
         # If it's in the database, return its name, else try to work out its name 
         if name != []:
             return name[0][0]
 
+        router = 0
         if '@' in mac:
             ip, mac = mac.split('@')
 
@@ -34,7 +36,7 @@ def get_device_name(mac: str, ip: str =None, device_db=DEFAULT_DB_NAME) -> str:
             try:
                 # First try getting the device name
                 name = socket.gethostbyaddr(ip)
-                add_device_to_database(connection, ip + '@' + mac, name[0])
+                add_device_to_database(connection, ip + '@' + mac, name=name[0], is_router=router)
                 return name[0]
             except socket.herror:
                 pass
@@ -48,23 +50,23 @@ def get_device_name(mac: str, ip: str =None, device_db=DEFAULT_DB_NAME) -> str:
         # Check if the MAC address is reserved for something like multicast
         reserved, reason = is_reserved_mac_address(mac)
         if reserved:
-            add_device_to_database(connection, mac, reason)
+            add_device_to_database(connection, mac, name=reason, is_router=router)
             return reason
         # If that fails, try getting the manufacturer from the MAC address
         manufacturer = get_manufacturer_from_mac(mac)
         if manufacturer != "":
-            add_device_to_database(connection, mac, manufacturer)
+            add_device_to_database(connection, mac, name=manufacturer, is_router=router)
             return manufacturer
         else:
-            add_device_to_database(connection, mac)
+            add_device_to_database(connection, mac, is_router=router)
             return mac
 
-def add_device_to_database(connection: sqlite3.Connection, mac: str, name='', os_guess=['No Guess', -1]):
-    with connection:
-        ensure_table_exists(connection)
+def add_device_to_database( mac: str, device_db=DEFAULT_DB_NAME, name='', os_guess=['No Guess', -1], is_router=0):
+    with sqlite3.connect(device_db) as connection:
+        ensure_device_table_exists(connection)
         existing_record = connection.execute("SELECT * FROM DEVICES WHERE mac = '{}';".format(mac))
         if existing_record.fetchall() == []:
-            connection.execute("INSERT OR REPLACE INTO DEVICES (mac, name, os, certainty) VALUES ('{}','{}','{}','{}');".format(mac, name, os_guess[0], os_guess[1]))
+            connection.execute("INSERT OR REPLACE INTO DEVICES (mac, name, os, certainty, is_router) VALUES ('{}','{}','{}','{}', {});".format(mac, name, os_guess[0], os_guess[1], is_router))
         else:
             update = ""
             if name != "":
@@ -75,5 +77,16 @@ def add_device_to_database(connection: sqlite3.Connection, mac: str, name='', os
             connection.execute("UPDATE DEVICES SET " + update.rstrip(',') + " WHERE mac = '{}';".format(mac))
 
 
+def is_in_router_list(mac: str, device_db=DEFAULT_DB_NAME) -> bool:
+    return mac in get_router_list(device_db=device_db)
+
+
+def get_router_list(device_db=DEFAULT_DB_NAME) -> list[str]:
+    with sqlite3.connect(device_db) as connection:
+        ensure_device_table_exists(connection)
+        response = connection.execute("SELECT mac FROM DEVICES WHERE is_router = 1;").fetchall()
+        return [r[0] for r in response]
+
+
 if __name__ == "__main__":
-    pass
+    get_router_list()
