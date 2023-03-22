63f9aaa6c355f59614d9ba3b0d76b8330eabda8b
Jacob <jacobtwwilson@gmail.com>
2023-03-22 13:07:16 +0000

    Add os guess to device

--- device.py
+++ device.py
@@ -1,4 +1,4 @@
-from device_db_manager import get_device_name
+from device_db_manager import get_device_name, get_os_guess
 
 class Device:
     """Represents a device with a MAC address as a primary key"""
@@ -8,6 +8,7 @@ class Device:
         self.devices_sent_to = {}
         self.devices_received_from = {} # TODO: Make MAC Address class
         self.name = get_device_name(mac_addr)
+        self.os_guess = get_os_guess(mac_addr)
 
     @staticmethod
     def add_packet_to_dict(dct: dict, mac: str, dest_port: int) -> dict:
